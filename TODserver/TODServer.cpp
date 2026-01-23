c58de2c0ad93bbaa5e1e025e13328a20a2b46bd7
Stephen Blythe <sblythe@lindenwood.edu>
2026-01-23 09:51:30 -0600

    Finished immediate response TOD server

--- TODserver/TODServer.cpp
+++ TODserver/TODServer.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int doServer(int);
 void doWork(int, struct sockaddr_in *);
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
   if (argc!=2)
     {
@@ -26,7 +26,7 @@ int main(int argc, char *argv)
 int doServer(int onPort)
 {
   // build a listening "socket"
-  int listen_socket = socket ( AF_INET, SOCK_STREAM, 0 );
+  int listen_sock = socket ( AF_INET, SOCK_STREAM, 0 );
 
   if (listen_sock<0)
     {
@@ -38,14 +38,63 @@ int doServer(int onPort)
   struct sockaddr_in local_addr;
 
   local_addr.sin_family = AF_INET;            //IPv4 address
-  local_addr.sin_sin_addr.s_addr = INADDR_ANY;//use any of my addresses
+  local_addr.sin_addr.s_addr = INADDR_ANY;//use any of my addresses
   local_addr.sin_port = htons (onPort);       //port for server to use
 
-  if (bind(listen_socket, (sockaddr *) &local_addr, sizeof(local_addr) ) )
+  if (bind(listen_sock, (sockaddr *) &local_addr, sizeof(local_addr) ) )
     {
       cerr << "bind() failed - possible causes include:" << endl
 	   << " * an invalid port number (already in use? restricted?)" << endl
 	   << " * an invalid local address (wildcard?)" << endl;
       return 1;
     }
+
+  if (listen(listen_sock, MAX_WAITERS )!=0)
+    {
+      cerr << "listen() failed" << endl;
+      return 1;
+    }
+
+  // handle one client each pass through loop 
+  while(true)
+    {
+      int connected_sock;       // id for incoming client socket
+      struct sockaddr_in from;  // address info from client
+      unsigned int from_len;    // byte count of incoming address
+
+      from_len = sizeof(from);
+
+      // now have a "live" socket connecting client to this server
+      connected_sock = accept(listen_sock,
+			      (struct sockaddr *) &from,
+			      &from_len);
+
+      doWork(connected_sock, &from); 
+    }
+}
+
+void doWork(int connsock, struct sockaddr_in *clientInfo)
+{
+  string buffer;
+
+  // build a string with the current time in it
+  time_t currTime = time(nullptr);
+  buffer = "current time is: ";
+  buffer+= ctime(&currTime);
+
+  char *cbuff = (char *) buffer.c_str();
+  int needed = buffer.length();
+  
+  
+  // send the contents of buffer back to client.
+  while (needed>0)
+    {
+      int numWritten = write(connsock, cbuff, needed);
+      needed -= numWritten;
+      cbuff += numWritten; 
+    }
+  // print a "log" message about the client
+  cout << "Connection from: " << inet_ntoa(clientInfo->sin_addr) << endl; 
+    
+  close(connsock); 
 }
